49ea40a3c56f5deab36cd12c11feaaa9e88e0cd5
siekiera <mistrzsiekiera@gmail.com>
2013-12-06 12:49:50 +0100

    Implementacja słuchania zmian w katalogach

--- isoman-notifier/src/main/java/pl/edu/pw/elka/mtoporow/isoman/notifier/NotifierInitValve.java
+++ isoman-notifier/src/main/java/pl/edu/pw/elka/mtoporow/isoman/notifier/NotifierInitValve.java
@@ -1,10 +1,14 @@
 package pl.edu.pw.elka.mtoporow.isoman.notifier;
 
+import org.jcontainer.dna.Logger;
 import org.objectledge.context.Context;
 import org.objectledge.pipeline.ProcessingException;
 import org.objectledge.pipeline.Valve;
 import org.objectledge.threads.ThreadPool;
+import pl.edu.pw.elka.mtoporow.isoman.common.config.ConfigManager;
 import pl.edu.pw.elka.mtoporow.isoman.notifier.tasks.NotifierTask;
+
+import java.io.IOException;
 
 /**
  * Valve inicjalizujący zadanie nasłuchujące zmiany w katalogach
@@ -15,16 +19,25 @@ import pl.edu.pw.elka.mtoporow.isoman.notifier.tasks.NotifierTask;
 public class NotifierInitValve implements Valve {
 
     private final ThreadPool threadPool;
+    private final ConfigManager configManager;
+    private final Logger logger;
     private NotifierTask notifierTask;
 
-    public NotifierInitValve(ThreadPool threadPool) {
+    public NotifierInitValve(ThreadPool threadPool, ConfigManager configManager, Logger logger) {
         this.threadPool = threadPool;
+        this.configManager = configManager;
+        this.logger = logger;
+        try {
+            //zainicjalizuj wątek powiadamiacza
+            notifierTask = new NotifierTask(configManager);
+            threadPool.runWorker(notifierTask);
+        } catch (IOException e) {
+            //FIXME
+        }
     }
 
     @Override
     public void process(Context context) throws ProcessingException {
-        //zainicjalizuj wątek powiadamiacza
-        notifierTask = new NotifierTask();
-        threadPool.runWorker(notifierTask);
+
     }
 }
